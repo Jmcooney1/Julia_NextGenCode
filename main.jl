940464bdc57a63f1695b8e8cc8df0bd7f13d49f8
Mary margaret Cooney <marymargaretcooney@marys-air-4.trinity.local>
2024-05-05 19:46:41 -0500

    Add the print_table function and it works

--- main.jl
+++ main.jl
@@ -1,5 +1,15 @@
-superHeroes = [("Hal Jordan","Green Lathern"), ("Bruce Wayne,Batman"),("Clark Kent","Superman"),
+superHeroes = [("Hal Jordan","Green Lathern"), ("Bruce Wayne","Batman"),("Clark Kent","Superman"),
 ("Diana Prince","Wonder Woman")]
 
 superHeroes_Dict = Dict(superHeroes)
 
+function print_superheroTable(table_dict)
+    print("| ")
+    printstyled(rpad("Civilian Name",3),color =:cyan)
+    print(" |")
+    printstyled(rpad("Superhero Name",3), color =:cyan)
+    println("")
+    for (civ_name,sup_name) in table_dict
+        println("|",rpad(civ_name,15), "|",rpad(sup_name,15),"|")
+    end
+end
